2d9458cfa4a5dc61d0de85817649f9b55cdcaee5
Eshel Eyni <esheleyni@gmail.com>
2025-11-29 06:38:42 +0200

    added fonts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/fonts/fonts.css";
+import "./style.css";
 
 import { createPinia } from "pinia";
 import { VueQueryPlugin } from "@tanstack/vue-query";
