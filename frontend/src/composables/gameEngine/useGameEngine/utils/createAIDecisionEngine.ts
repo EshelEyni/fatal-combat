37cfd7d38be44a7f27b28a83bd2cc71bd38a99e6
Eshel Eyni <esheleyni@gmail.com>
2025-12-07 00:20:42 +0200

    fix AI logic 2

--- frontend/src/composables/gameEngine/useGameEngine/utils/createAIDecisionEngine.ts
+++ frontend/src/composables/gameEngine/useGameEngine/utils/createAIDecisionEngine.ts
@@ -1,6 +1,8 @@
 import type { Fighter } from "../../../../classes/Fighter";
+import { isHitDetected } from "./attack";
 
-const reaction = 30;
+// const jumpChance = 0.75;
+const reaction = 12;
 
 type CreateAIDecisionEngineParams = { userFighter: Fighter; cpuFighter: Fighter };
 
@@ -11,18 +13,27 @@ export const createAIDecisionEngine = ({
    let frame = 0;
 
    function update(frame: number) {
+      const playerIsLeft = userFighter.position.x < cpuFighter.position.x;
       const attackChance = Math.random() < 0.1;
       const isTakingHit =
          cpuFighter.image === cpuFighter.sprites.takeHit.image && cpuFighter.framesCurrent <= 2;
 
-      const playerIsLeft = userFighter.position.x < cpuFighter.position.x;
+      const isAttackWouldHit = isHitDetected({
+         rectangle1: cpuFighter.attackBox,
+         rectangle2: {
+            position: userFighter.position,
+            width: userFighter.width,
+            height: userFighter.height,
+         },
+      });
+
       const distance = Math.abs(userFighter.position.x - cpuFighter.position.x);
       const deadzone = cpuFighter.attackBox.width * 0.8;
-      const enemyIsInAttackRange = deadzone - distance >= 0;
+      console.log(cpuFighter.attackBox.width);
 
       if (frame % reaction !== 0 || cpuFighter.dead) return;
 
-      if (!enemyIsInAttackRange) {
+      if (distance > deadzone) {
          cpuFighter.velocity.x = playerIsLeft ? -8 : 8;
          cpuFighter.switchSprite("run");
       } else {
@@ -30,7 +41,11 @@ export const createAIDecisionEngine = ({
          cpuFighter.switchSprite("idle");
       }
 
-      if (enemyIsInAttackRange && attackChance && !isTakingHit) {
+      //   if (Math.random() < jumpChance) {
+      //      enemy.velocity.y = -20;
+      //   }
+
+      if (isAttackWouldHit && attackChance && !isTakingHit) {
          cpuFighter.attack();
          cpuFighter.velocity.x = 0;
       }
