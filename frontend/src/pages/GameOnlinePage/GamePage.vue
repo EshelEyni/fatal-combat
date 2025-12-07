f126fc00e01271120a0f707084beaa55c7e59066
Eshel Eyni <esheleyni@gmail.com>
2025-12-07 19:11:05 +0200

    sockets supports - #2

--- frontend/src/pages/GameOnlinePage/GamePage.vue
+++ frontend/src/pages/GameOnlinePage/GamePage.vue
@@ -1,9 +1,26 @@
 <template></template>
 
 <script setup lang="ts">
+import { onMounted, watch } from "vue";
+import { useLoginWithToken } from "../../composables/auth/useLoginWithToken";
 import { useWebSocket } from "../../composables/useWebsockets";
+const url = "ws://localhost:8000/ws";
 
-const { socket, send } = useWebSocket("ws://localhost:8000/ws");
+const { loggedInUser } = useLoginWithToken();
+const { socket, send } = useWebSocket(url);
+
+onMounted(() => {
+   if (!loggedInUser.value) return;
+
+   socket.value.onopen = () => {
+      const stringifedMsg = JSON.stringify({
+         type: "join_lobby",
+         username: loggedInUser.value.id,
+      });
+
+      socket.value.send(stringifedMsg);
+   };
+});
 </script>
 
 <style scoped>
