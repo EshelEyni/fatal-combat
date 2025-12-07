1fb3541f736f5cfafef044346214e92ee4514ac3
Eshel Eyni <esheleyni@gmail.com>
2025-12-08 01:03:23 +0200

    sockets supports - #6 (removeunused hook)

--- frontend/src/pages/GameOnlinePage/GamePage.vue
+++ frontend/src/pages/GameOnlinePage/GamePage.vue
@@ -1,7 +1,14 @@
 <template>
    <div v-if="currentScreen === 'lobby'">
       <ul>
-         <li v-for="user in onlineUsers" :key="user.id">{{ user.username }}</li>
+         <li
+            v-for="user in onlineUsers"
+            :key="user.id"
+            @click="onInviteToGame({ fromUserId: loggedInUser?.id, toUserId: user.id })"
+            class="hover:underline cursor-pointer"
+         >
+            {{ user.username }}
+         </li>
       </ul>
    </div>
 </template>
@@ -10,10 +17,25 @@
 import { ref } from "vue";
 import { useOnlineUsersStore } from "../../store/users";
 import { storeToRefs } from "pinia";
+import { useWebSocketStore } from "../../store/websocket";
+import { useLoginWithToken } from "../../composables/auth/useLoginWithToken";
+
+const webSocketStore = useWebSocketStore();
 const onlineUsersStore = useOnlineUsersStore();
 const { onlineUsers } = storeToRefs(onlineUsersStore);
+const { loggedInUser } = useLoginWithToken();
 
 const currentScreen = ref<"lobby" | "game">("lobby");
+
+const onInviteToGame = (data: any) => {
+   if (!webSocketStore.connectionStatus) return;
+   if (!data.fromUserId || !data.toUserId) return;
+   webSocketStore.send({
+      type: "send_game_invite",
+      fromUserId: data.fromUserId,
+      toUserId: data.toUserId,
+   });
+};
 </script>
 
 <style scoped>
