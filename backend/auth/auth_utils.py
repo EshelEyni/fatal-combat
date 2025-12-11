ceef6eb330992a684258178250ae92a8427fb34b
Eshel Eyni <esheleyni@gmail.com>
2025-12-11 19:08:53 +0200

    added env var

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -1,13 +1,19 @@
+import os
 from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from jose import jwt
+from dotenv import load_dotenv
 
-SECRET_KEY = "3124555"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()  # loads .env file
 
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
-pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=[os.getenv("PASSWORD_HASH_SCHEME", "bcrypt_sha256")],
+    deprecated=os.getenv("PASSWORD_HASH_DEPRECATED", "auto"),
+)
 
 
 def hash_password(password: str):
@@ -22,7 +28,6 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
