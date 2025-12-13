fa143a9273ac7f911210bb54c8d26f40953f5b9f
Eshel Eyni <esheleyni@gmail.com>
2025-12-13 02:47:45 +0200

    fixed ws base url

--- frontend/src/services/utils/getBaseServerUrl.ts
+++ frontend/src/services/utils/getBaseServerUrl.ts
@@ -2,8 +2,8 @@ export const getBaseServerUrl = (type: "api" | "websocket"): string => {
    switch (type) {
       case "websocket":
          return import.meta.env.PROD
-            ? `wss://${window.location.host}/ws/`
-            : "ws://localhost:8000/ws/";
+            ? `wss://${window.location.host}/ws`
+            : "ws://localhost:8000/ws";
       case "api":
       default:
          return import.meta.env.PROD ? "/api/" : "http://localhost:8000/api/";
