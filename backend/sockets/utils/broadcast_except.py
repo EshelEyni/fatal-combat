7858dea906d90f868cbf519914dfe4a5ee63a894
Eshel Eyni <esheleyni@gmail.com>
2025-12-13 03:21:10 +0200

    remove comments

--- backend/sockets/utils/broadcast_except.py
+++ backend/sockets/utils/broadcast_except.py
@@ -5,9 +5,6 @@ from ..state import active_users
 
 
 async def broadcast_except(message, exclude_ws: WebSocket):
-
-    print("ACTIVE USERS:", list(active_users.keys()))
-
     data = json.dumps(message)
     for info in active_users.values():
         ws = info["ws"]
--- backend/sockets/utils/send_to.py
+++ backend/sockets/utils/send_to.py
@@ -2,7 +2,6 @@ from fastapi import WebSocket
 
 
 async def send_to(ws: WebSocket, message: dict):
-    """Send a JSON message to a single websocket."""
     try:
         await ws.send_json(message)
     except Exception as e:
